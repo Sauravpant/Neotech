4e5d2a8c46aba9b3ca16a5cf3c4488bd8b1c050e
Saurav Pant <sauravpant777@gmail.com>
2025-10-12 14:55:50 +0545

    feat: update database connection and add  Stripe configuration

---
+++ backend/src/configs/db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`);
+    console.log(`MongoDB connected... Connection host ${connectionInstance.connection.host}`);
+  } catch (err: any) {
+    console.error("Failed connecting the MongoDB...");
+    process.exit(1)
+  }
+};
+
+export default connectDB;
