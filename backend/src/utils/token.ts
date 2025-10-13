6318430417a1e0e9b935c06e08fa901b1b00497b
Saurav Pant <sauravpant777@gmail.com>
2025-10-13 23:08:06 +0545

    feat: extend access token expiration

--- backend/src/utils/token.ts
+++ backend/src/utils/token.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const generateAccessToken = async (_id: string, role: string) => {
-  return jwt.sign({ _id, role }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
+  return jwt.sign({ _id, role }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30m" });
 };
 
 export const generateRefreshToken = async (_id: string, role: string) => {
