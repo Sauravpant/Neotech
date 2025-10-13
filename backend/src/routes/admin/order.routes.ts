83762751be7dd2000ce873576c996828792df8c6
Saurav Pant <sauravpant777@gmail.com>
2025-10-13 22:31:58 +0545

    feat: add authentication middleware to order routes

--- backend/src/routes/admin/order.routes.ts
+++ backend/src/routes/admin/order.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { deleteOrder, getAllOrders, getOrderStats, updateOrderStatus } from "../../controllers/admin/order.controller";
+import { verifyAdmin } from "../../middlewares/verify-admin.middleware";
+import { verifyJWT } from "../../middlewares/auth.middlewares";
 
 const router = Router();
+
+router.use(verifyJWT, verifyAdmin);
 
 router.get("/get-all-orders", getAllOrders);
 router.patch("/update-order/:orderId", updateOrderStatus);
