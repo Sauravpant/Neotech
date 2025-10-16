f6591ce83a34c8f30ad65b3cbfc6e23a83d6c091
Saurav Pant <sauravpant777@gmail.com>
2025-10-17 00:41:27 +0545

    fix: correct secret token for  refresh token

--- backend/src/utils/token.ts
+++ backend/src/utils/token.ts
@@ -5,5 +5,5 @@ export const generateAccessToken = async (_id: string, role: string) => {
 };
 
 export const generateRefreshToken = async (_id: string, role: string) => {
-  return jwt.sign({ _id, role }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "2d" });
+  return jwt.sign({ _id, role }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "2d" });
 };
