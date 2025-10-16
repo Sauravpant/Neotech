0763ba90cf8d4e050cd538640820d9a259a38890
Saurav Pant <sauravpant777@gmail.com>
2025-10-16 15:30:22 +0545

    feat: update routes

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,13 +1,38 @@
-import UserLayout from "@/components/user/layout/UserLayout";
+import UserLayout from "@/components/layout/UserLayout";
+import LoginPage from "@/pages/auth/LoginPage";
+import SignupPage from "@/pages/auth/SignupPage";
+import CategoryPage from "@/pages/user/CategoryPage";
 import HomePage from "@/pages/user/HomePage";
 import { Routes, Route } from "react-router-dom";
+import ProtectedRoutes from "./ProtectedRoutes";
+import UserSettingsLayout from "@/components/layout/UserSettingsLayout";
+import ProfileSettingsPage from "@/pages/user/ProfileSettingsPage";
+import AccountSettingsPage from "@/pages/user/AccountSettingsPage";
+import ProductsPage from "@/pages/user/ProductsPage";
+import ProductDetailsPage from "@/pages/user/ProductDetailsPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<UserLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="/categories" element={<CategoryPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="product/:id" element={<ProductDetailsPage />} />
       </Route>
+
+      <Route element={<UserLayout />}>
+        <Route element={<ProtectedRoutes allowedRole="user" />}>
+          <Route path="/settings" element={<UserSettingsLayout />}>
+            <Route index element={<ProfileSettingsPage />} />
+            <Route path="account" element={<AccountSettingsPage />} />
+          </Route>
+        </Route>
+      </Route>
+
+      {/* Authentication Routes */}
+      <Route path="/auth/login" element={<LoginPage />} />
+      <Route path="/auth/signup" element={<SignupPage />} />
     </Routes>
   );
 };
