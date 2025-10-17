299f1ba3417848976a6911bcefa8af925db5c6f6
Saurav Pant <sauravpant777@gmail.com>
2025-10-17 23:48:32 +0545

    feat: add utility functions

---
+++ frontend/src/lib/helpers.ts
@@ -0,0 +1,14 @@
+export const formatDate = (date: Date) => {
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  }).format(new Date(date));
+};
+
+export const formatPrice = (price: number) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(price);
+};
